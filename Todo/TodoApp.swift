9b260f6c96d88f53f3c922e78e0d644c29ffee26
ddduc02 <ducducdo2002@gmail.com>
2023-08-04 22:46:54 +0700

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by DucDo on 04/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
